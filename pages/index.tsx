2cc5493ad5aa213a711eba149b7687a85200a1b2
giakhang94 <ngk.khang94@gmail.com>
2024-01-07 13:08:42 +0700

    change home page layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,17 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
     >
-      <h1>Hello World!</h1>
+      <h1 className="text-3xl font-semibold text-slate-600 mb-20">
+        NextJS - Typescript - Dev Blog Mini Project
+      </h1>
+      <a
+        className="bg-blue-500 text-white text-lg font-sembold py-2 px-5 rounded-md"
+        href="https://next-ts-dev-blogs.vercel.app/blogs"
+      >
+        Go To Blogs
+      </a>
     </main>
   );
 }
